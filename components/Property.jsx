b92250094100d8f87ec2ba52d7635906b455f316
Ukaypromise <promiseukaimo@gmail.com>
2023-03-12 00:53:47 +0100

    Add styling

--- components/Property.jsx
+++ components/Property.jsx
@@ -1,6 +1,6 @@
 import Link from "next/link";
 import Image from "next/image";
-import { Box, Flex, Text, Avatar } from "@chakra-ui/react";
+import { Box, Flex, Text, Avatar, Card, CardBody } from "@chakra-ui/react";
 import { FaBed, FaBath  } from "react-icons/fa";
 import { BsGridFill } from "react-icons/bs";
 import { GoVerified } from "react-icons/go";
@@ -30,43 +30,53 @@ const Property = ({
       justifyContent="flex-start"
       cursor="pointer"
     >
-      <Box>
-        <Image
-          src={coverPhoto ? coverPhoto.url : DefaultImage}
-          width={400}
-          height={260}
-          alt="image"
-        />
-      </Box>
-      <Box w="full">
-        <Flex paddingTop="2" alignItems="center" justifyContent="space-between">
-          <Flex alignItems="center">
-            <Box paddingRight="3" color="green.400">
-              {isVerified && <GoVerified />}
-            </Box>
-            <Text fontWeight="bold" fontSize="lg">
-              AED {price}
-              {rentFrequency && `/${rentFrequency}`}
-            </Text>
-          </Flex>
+      <Card maxW="sm">
+        <CardBody>
           <Box>
-            <Avatar size="sm" src={agency?.logo?.url}></Avatar>
+            <Image
+              src={coverPhoto ? coverPhoto.url : DefaultImage}
+              width={400}
+              height={260}
+              alt="image"
+              borderRadius="lg"
+            />
           </Box>
-        </Flex>
-        <Flex
-          alignItems="center"
-          p="1"
-          justifyContent="space-between"
-          w="250px"
-          color="blue.400"
-        >
-          {rooms}
-          <FaBed /> | {baths} <FaBath /> | {millify(area)} sqft <BsGridFill />
-        </Flex>
-        <Text fontSize="lg">
-          {title.length > 30 ? title.substring(0, 30) + "..." : title}
-        </Text>
-      </Box>
+          <Box w="full">
+            <Flex
+              paddingTop="2"
+              alignItems="center"
+              justifyContent="space-between"
+            >
+              <Flex alignItems="center">
+                <Box paddingRight="3" color="green.400">
+                  {isVerified && <GoVerified />}
+                </Box>
+                <Text fontWeight="bold" fontSize="lg">
+                  $ {price}
+                  {rentFrequency && `/${rentFrequency}`}
+                </Text>
+              </Flex>
+              <Box>
+                <Avatar size="sm" src={agency?.logo?.url}></Avatar>
+              </Box>
+            </Flex>
+            <Flex
+              alignItems="center"
+              p="1"
+              justifyContent="space-between"
+              w="250px"
+              color="blue.400"
+            >
+              {rooms}
+              <FaBed /> | {baths} <FaBath /> | {millify(area)} sqft{" "}
+              <BsGridFill />
+            </Flex>
+            <Text fontSize="lg">
+              {title.length > 30 ? title.substring(0, 30) + "..." : title}
+            </Text>
+          </Box>
+        </CardBody>
+      </Card>
     </Flex>
   </Link>
 );
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 import Image from "next/image";
 import Link from "next/link";
-import { Flex, Box, Text, Button } from "@chakra-ui/react";
+import { Flex, Box, Text, Button, Card, CardBody, Divider } from "@chakra-ui/react";
 import { baseUrl, fetchApi } from "@/utils/fetchApi";
 import Property from "../components/Property"
 
@@ -17,8 +17,18 @@ const Banner = ({
 }) => {
   return (
     <Flex flexWrap="wrap" justifyContent="center" alignItems="center" m="10">
-      <Image src={imageUrl} width={500} height="300" alt="banner" />
-      <Box>
+      <Card maxW="sm" >
+        <CardBody>
+          <Image
+            src={imageUrl}
+            width={500}
+            height="300"
+            alt="banner"
+            borderradius="lg"
+          />
+        </CardBody>
+      </Card>
+      <Box paddingLeft="15">
         <Text color="grey.500" fontSize="sm" fontWeight="medium">
           {purpose}
         </Text>
@@ -28,7 +38,7 @@ const Banner = ({
         <Text color="grey.700" fontSize="lg" paddingTop="3" paddingBottom="3">
           {desc1} <br /> {desc2}
         </Text>
-        <Button fontSize="xl" color="white">
+        <Button fontSize="xl" color="red">
           <Link href={linkName}>{buttonText} </Link>
         </Button>
       </Box>
@@ -40,32 +50,37 @@ export default function Home({ propertyForSale, propertyForRent }) {
   
   return (
     <Box>
-      <Banner
-        purpose="BUY OR RENT A HOME"
-        title1="Rental Homes for"
-        title2="Everyone"
-        desc1="Explore Apartments, Villas, Homes"
-        desc2="and more."
-        buttonText="Explore Renting"
-        linkName="/search?purpose=for-rent"
-        imageUrl="https://images.unsplash.com/photo-1626178793926-22b28830aa30?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8cHJvcGVydHl8ZW58MHx8MHx8&auto=format&fit=crop&w=800&q=60"
-      />
-      <Flex flexWrap="wrap">
+      <Flex>
+        <Banner
+          purpose="BUY A HOME"
+          title1="Homes for"
+          title2="Everyone"
+          desc1="Explore Apartments, Villas, Homes"
+          desc2="and more."
+          buttonText="Explore"
+          linkName="/search?purpose=for-rent"
+          imageUrl="https://images.unsplash.com/photo-1626178793926-22b28830aa30?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8cHJvcGVydHl8ZW58MHx8MHx8&auto=format&fit=crop&w=800&q=60"
+        />
+
+        <Banner
+          purpose="Rent A HOME"
+          title1="Get Your "
+          title2="Dream Home "
+          desc1="Explore Apartments, Villas, Homes"
+          desc2="and more."
+          buttonText="Explore"
+          linkName="/search?purpose=for-sale"
+          imageUrl="https://images.unsplash.com/photo-1560518883-ce09059eeffa?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8cHJvcGVydHl8ZW58MHx8MHx8&auto=format&fit=crop&w=800&q=60"
+        />
+      </Flex>
+      <Divider p="2" />
+      <Flex flexWrap="wrap" marginTop="10">
         {propertyForRent.map((property) => (
           <Property key={property.id} property={property} />
         ))}
       </Flex>
-      <Banner
-        purpose="SELL A HOME"
-        title1="Sell or Rent a Home "
-        title2="Dream Home "
-        desc1="Explore Apartments, Villas, Homes"
-        desc2="and more."
-        buttonText="Explore Buying"
-        linkName="/search?purpose=for-sale"
-        imageUrl="https://images.unsplash.com/photo-1560518883-ce09059eeffa?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8cHJvcGVydHl8ZW58MHx8MHx8&auto=format&fit=crop&w=800&q=60"
-      />
-      <Flex flexWrap="wrap">
+      <Divider />
+      <Flex flexWrap="wrap" marginTop="10">
         {propertyForSale.map((property) => (
           <Property key={property.id} property={property} />
         ))}
